0004ea7160d5c7235fb6b0d25e641850a98d41a1
ethancrypto <jonathan.cryptoguru@gmail.com>
2021-12-20 18:04:36 -0500

    init

---
+++ src/views/Wrapper/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Wrapper extends React.PureComponent {
+  render() {
+    return (
+      <div className="bg-blue-500">Wrapper!</div>
+    )
+  }
+}
+
+export default Wrapper
