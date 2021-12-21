94bb50a40e50a25f307ec734b2726e37b950c57d
legend524 <thecrazydev524@gmail.com>
2021-12-20 23:22:36 -0500

    built out some components

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,12 @@
+import Landing from './Landing'
+import MyDomains from './MyDomains'
+import SunriseAuction from './SunriseAuction'
 import Wrapper from './Wrapper'
 
 const views = {
+  Landing,
+  MyDomains,
+  SunriseAuction,
   Wrapper,
 }
 
