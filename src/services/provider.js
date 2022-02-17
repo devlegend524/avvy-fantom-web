ccf2ba50f925fee8f38a04bd83533b91824ec212
legend524 <thecrazydev524@gmail.com>
2022-02-16 21:20:22 -0500

    fixing connection for metamask mobile

--- src/services/provider.js
+++ src/services/provider.js
@@ -27,6 +27,11 @@ const provider = {
 
   EVENTS,
 
+  _getChainId: async () => {
+    const raw = await window.ethereum.request({ method: 'eth_chainId' })
+    return parseInt(raw, 16)
+  },
+
   // connect to web3 via metamask
   connectMetamask: () => {
     return new Promise(async (resolve, reject) => {
@@ -40,7 +45,7 @@ const provider = {
       }
 
       // verify they connected to the right chain
-      const chainId = parseInt(window.ethereum.chainId, 16)
+      const chainId = await provider._getChainId()
       const expectedChainId = parseInt(services.environment.DEFAULT_CHAIN_ID)
       if (chainId !== expectedChainId) {
         try {
@@ -51,34 +56,29 @@ const provider = {
               chainId: '0x' + expectedChainId.toString(16)
             }]
           })
-          waitForChainChanged(expectedChainId)
+          window.location.reload()
         } catch (err) {
-          if (err.code === 4902) {
-            services.logger.info('Chain not found')
-            try {
-              services.logger.info('Attempting to add chain')
-              await window.ethereum.request({
-                method: 'wallet_addEthereumChain',
-                params: [
-                  {
-                    chainId: '0x' + expectedChainId.toString(16),
-                    chainName: services.environment.DEFAULT_CHAIN_NAME,
-                    nativeCurrency: {
-                      name: 'AVAX',
-                      symbol: 'AVAX',
-                      decimals: 18,
-                    },
-                    rpcUrls: [services.environment.DEFAULT_PROVIDER_URL],
-                    blockExplorerUrls: [services.environment.DEFAULT_BLOCK_EXPLORER_URL],
-                  }
-                ]
-              })
-              waitForChainChanged(expectedChainId)
-            } catch (err) {
-              services.logger.error(err)
-              return reject('WRONG_CHAIN')
-            }
-          } else {
+          services.logger.info('Chain not found')
+          try {
+            services.logger.info('Attempting to add chain')
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: '0x' + expectedChainId.toString(16),
+                  chainName: services.environment.DEFAULT_CHAIN_NAME,
+                  nativeCurrency: {
+                    name: 'AVAX',
+                    symbol: 'AVAX',
+                    decimals: 18,
+                  },
+                  rpcUrls: [services.environment.DEFAULT_PROVIDER_URL],
+                  blockExplorerUrls: [services.environment.DEFAULT_BLOCK_EXPLORER_URL],
+                }
+              ]
+            })
+            window.location.reload()
+          } catch (err) {
             services.logger.error(err)
             return reject('WRONG_CHAIN')
           }
@@ -88,7 +88,7 @@ const provider = {
       }
 
       async function waitForChainChanged(expectedChainId) {
-        const chainId = parseInt(window.ethereum.chainId, 16)
+        const chainId = await provider._getChainId()
         services.logger.info('Waiting for chain to change')
         if (chainId === expectedChainId) {
           continueInitialization()
@@ -98,6 +98,7 @@ const provider = {
       }
 
       async function continueInitialization() {
+        _chainId = await provider._getChainId()
         services.logger.info('Chain has changed')
         services.logger.info('Initializing accounts')
         const accounts = await window.ethereum.request({
@@ -138,7 +139,7 @@ const provider = {
   // get api client
   buildAPI: () => {
     if (_isConnected) {
-      _chainId = window.ethereum.chainId ? parseInt(window.ethereum.chainId, 16) : parseInt(services.environment.DEFAULT_CHAIN_ID, 16) 
+      // _chainId set when we connect
       _provider = new ethers.providers.Web3Provider(window.ethereum)
       _signer = _provider.getSigner()
       return new API(_chainId, _account, _signer);
