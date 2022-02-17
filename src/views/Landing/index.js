ee02b2738a0746708aa133cb4d40f621450fdd93
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-02-17 12:12:50 -0500

    add account requirements for auction

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -17,15 +17,15 @@ class Landing extends React.PureComponent {
           <components.DomainSearch />
         </div>
         <Link
-          to={services.linking.path('SunriseAuction')}
-          className="flex justify-between bg-gray-100 font-bold p-4 rounded mb-2">
-          <div>{'Sunrise Auction'}</div>
-          <ArrowRightIcon className="h-6" />
-        </Link>
-        <Link
           to={services.linking.path('MyDomains')}
           className="flex justify-between bg-gray-100 font-bold p-4 rounded mb-2">
           <div>{'My Domains'}</div>
+          <ArrowRightIcon className="h-6" />
+        </Link>
+        <Link
+          to={services.linking.path('SunriseAuction')}
+          className="flex justify-between bg-gray-100 font-bold p-4 rounded mb-2">
+          <div>{'Sunrise Auction'}</div>
           <ArrowRightIcon className="h-6" />
         </Link>
       </div>
