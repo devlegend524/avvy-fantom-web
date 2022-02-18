ef64c147a391c33ce746a132105b21c900b23728
legend524 <thecrazydev524@gmail.com>
2022-02-18 01:29:59 -0500

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,6 +7,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './store'
+
+import * as Sentry from "@sentry/browser"
+import { BrowserTracing } from "@sentry/tracing"
+
+import services from 'services'
+
+Sentry.init({
+  dsn: services.environment.SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
