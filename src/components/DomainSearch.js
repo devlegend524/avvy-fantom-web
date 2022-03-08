63d2bab71b0d2b50f3badc026efcbe5a582b1da8
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-03-07 20:54:10 -0500

    make search glass into a button; fixes #11

--- src/components/DomainSearch.js
+++ src/components/DomainSearch.js
@@ -26,7 +26,7 @@ function DomainSearch(props) {
       <form onSubmit={handleSubmit}>
         <input autoComplete="off" ref={textInput} autoCapitalize="off" placeholder={props.placeholder || 'Search domain names'} className='bg-transparent w-full placeholder:text-gray-400 text-black dark:text-gray-300 text-center p-4' />
       </form>
-      <div className='absolute right-0 top-0 h-full flex items-center justify-center mr-4 pointer-events-none'>
+      <div className='absolute right-0 top-0 h-full flex items-center justify-center mr-4 cursor-pointer' onClick={handleSubmit}>
         <SearchIcon className='w-6 text-gray-300' />
       </div>
     </div>
