a730b2e8fdd914f7082007144d7aa2d77b4de0aa
legend524 <thecrazydev524@gmail.com>
2022-04-20 21:19:33 -0400

    update privacy selectin to not be checkbox; #57

--- src/components/DomainPrivacy.js
+++ src/components/DomainPrivacy.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { CheckCircleIcon } from '@heroicons/react/solid'
 
 
 class DomainPrivacy extends React.PureComponent {
   render() {
+    const error = this.props.error
     return (
       <>
         <div className='grid gap-2 grid-cols-1 md:grid-cols-2 w-full'>
-          <div className='bg-gray-100 dark:bg-gray-800 p-4 rounded-xl'>
-            <div className='font-bold'>{'Standard Privacy'}</div>
+          <div onClick={() => this.props.onCheck(false)} className={`cursor-pointer bg-gray-100 dark:bg-gray-800 p-4 rounded-xl border-2 ${error ? 'border-red-500' : this.props.isEnhancedPrivacy ? 'border-gray-100 dark:border-gray-800' : 'border-gray-700 dark:border-white'}`}>
+            <div className='font-bold flex items-center justify-between'><div className=''>{'Standard Privacy'}</div><div className={this.props.isEnhancedPrivacy ? 'hidden' : ''}><CheckCircleIcon className='w-6 text-gray-700 dark:text-white' /></div></div>
             <ul className='list-disc pl-4 mt-4 text-sm'>
               <li>{'Anyone on the blockchain can read your domain names'}</li>
               <li>{'NFT exchanges can list your domain names'}</li>
@@ -16,8 +18,8 @@ class DomainPrivacy extends React.PureComponent {
               <li>{'You cannot switch these names to Enhanced Privacy***'}</li>
             </ul>
           </div>
-          <div className='bg-gray-100 dark:bg-gray-800 p-4 rounded-xl'>
-            <div className='font-bold'>{'Enhanced Privacy'}</div>
+          <div onClick={() => this.props.onCheck(true)} className={`cursor-pointer bg-gray-100 dark:bg-gray-800 p-4 rounded-xl border-2 ${error ? 'border-red-500' : this.props.isEnhancedPrivacy ? 'border-gray-700 dark:border-white' : 'border-gray-100 dark:border-gray-800'}`}>
+            <div className='font-bold flex items-center justify-between'><div className=''>{'Enhanced Privacy'}</div><div className={this.props.isEnhancedPrivacy ? '' : 'hidden'}><CheckCircleIcon className='w-6 text-gray-700 dark:text-white' /></div></div>
             <ul className='list-disc pl-4 mt-4 text-sm'>
               <li>{'Internet users cannot read your domain names*'}</li>
               <li>{'NFT exchanges cannot list your domain names*'}</li>
