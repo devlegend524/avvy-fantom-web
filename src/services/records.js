05649ddbc40f4458d90698e3acd213e397585b68
legend524 <thecrazydev524@gmail.com>
2022-07-04 16:32:25 -0400

    add more information for records

--- src/services/records.js
+++ src/services/records.js
@@ -1,18 +1,13 @@
-const records = {
-  standard: [
-    { name: 'X-Chain Address', type: 1 },
-    { name: 'P-Chain Address', type: 2 },
-    { name: 'C-Chain / EVM Address', type: 3 },
-    { name: 'Validator NodeID', type: 4 },
-    { name: 'DNS CNAME Record', type: 5 },
-    { name: 'DNS A Record', type: 6 },
-    { name: 'Avatar', type: 7 },
-  ],
+import services from 'services'
 
-  getStandard: (type) => {
-    for (let i = 0; i < records.standard.length; i += 1) {
-      if (records.standard[i].type === type) return records.standard[i]
-    }
+const records = {
+  list: async () => {
+    const api = await services.provider.buildAPI()
+    return api.avvy.RECORDS._LIST
+  },
+  getStandard: async (name) => {
+    const api = await services.provider.buildAPI()
+    return api.avvy.records[name]
   }
 }
 
